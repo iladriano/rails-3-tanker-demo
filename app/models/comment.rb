b511697725ddb07d7412df374109c1e718527939
Adrián <adrian@indextank.com>
2011-06-13 19:12:09 -0300

    Change index name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   include Tanker
-  tankit 'rails_3_demo' do  
+  tankit 'rails_3_tanker_demo' do  
     indexes :commenter
     indexes :postname
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   include Tanker
-  tankit 'rails_3_demo' do
+  tankit 'rails_3_tanker_demo' do
     indexes :title
   end
 end
